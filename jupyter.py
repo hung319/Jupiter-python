df4ff3eb84bca5d10e3f4925ae805f236f0c0bc8
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2024-12-12 10:48:34 +0700

    Update jupyter.py

--- jupyter.py
+++ jupyter.py
@@ -3,16 +3,25 @@ import subprocess
 
 def install_jupyter():
     try:
-        # Cài đặt Jupyter Lab
-        subprocess.check_call([os.sys.executable, '-m', 'pip', 'install', '-r', 're.txt'])
-        print("Jupyter Lab đã được cài đặt thành công.")
+        # Kiểm tra nếu JupyterLab đã được cài đặt
+        subprocess.check_call([os.sys.executable, '-m', 'jupyter', '--version'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        print("Jupyter Lab đã được cài đặt trước đó.")
     except subprocess.CalledProcessError:
-        print("Có lỗi xảy ra trong quá trình cài đặt Jupyter Lab.")
+        print("Jupyter Lab chưa được cài đặt. Đang tiến hành cài đặt...")
+        try:
+            subprocess.check_call([os.sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+            print("Jupyter Lab đã được cài đặt thành công.")
+        except subprocess.CalledProcessError as e:
+            print(f"Có lỗi xảy ra trong quá trình cài đặt Jupyter Lab: {e}")
 
 def run_jupyter():
     try:
         # Chạy Jupyter Lab
-        subprocess.run([os.sys.executable, '-m', "jupyter", "lab", "--ip=0.0.0.0", "--port=80", "--no-browser", "--allow-root", "--NotebookApp.token='11042006'"])
+        subprocess.run([
+            os.sys.executable, '-m', "jupyter", "lab", 
+            "--ip=0.0.0.0", "--port=8888", "--no-browser", 
+            "--allow-root", "--NotebookApp.token='11042006'"
+        ])
     except KeyboardInterrupt:
         print("Jupyter Lab đã dừng.")
 
